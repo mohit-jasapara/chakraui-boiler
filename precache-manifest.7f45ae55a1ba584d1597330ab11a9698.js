c59b1f1c23285a15ee10a28fb3f0e9e48e43f7a9
Mohit Jasapara <mohitjasapara@gmail.com>
2020-10-26 16:00:28 +0530

    Updates

---
+++ precache-manifest.7f45ae55a1ba584d1597330ab11a9698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883c5c55e3bd768d6efc0269d28100b9",
+    "url": "/chakraui-boiler/index.html"
+  },
+  {
+    "revision": "fdbe86ad50b4c2688def",
+    "url": "/chakraui-boiler/static/js/2.60244517.chunk.js"
+  },
+  {
+    "revision": "84f2c4e15f0737796d976a22ee26a693",
+    "url": "/chakraui-boiler/static/js/2.60244517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ee94fb303af8868970",
+    "url": "/chakraui-boiler/static/js/main.fbc613ea.chunk.js"
+  },
+  {
+    "revision": "4d4a6e930f7120094391",
+    "url": "/chakraui-boiler/static/js/runtime-main.f4f83a80.js"
+  }
+]);
